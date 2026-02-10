fefe8f18f18bd7d6b80997830ff565f073e3b327
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2026-02-10 17:00:07 -0500

    0.0.7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export { assignGingerly } from './assignGingerly.js';
 export { assignTentatively } from './assignTentatively.js';
 export { BaseRegistry } from './assignGingerly.js';
+export { waitForEvent } from './waitForEvent.js';
 import './object-extension.js';
